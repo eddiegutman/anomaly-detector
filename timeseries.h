b25703f36630fc09373ff2e308477e659fe4dc43
Eddie Gutman <edigutman@gmail.com>
2020-11-25 20:52:40 +0200

    Updated timeseries.h
    Created a constructor for timeseries

--- timeseries.h
+++ timeseries.h
@@ -1,19 +1,31 @@
-
-
 #ifndef TIMESERIES_H_
 #define TIMESERIES_H_
 
-using namespace std;
+#include <vector>
+#include <fstream>
+#include <sstream>
 
-class TimeSeries{
+class TimeSeries {
 
 public:
+    std::vector<std::vector<std::string>> Lines;
 
-	TimeSeries(const char* CSVfileName){
-	}
+    TimeSeries(const char *CSVfileName) {
+        std::ifstream file;
+        file.open(CSVfileName);
+        std::string line, cell;
+        while (std::getline(file, line)) {
+            std::vector<std::string> row;
+            std::stringstream ss(line);
+            while (std::getline(ss, cell, ',')){
+                row.push_back(cell);
+            }
+            Lines.push_back(row);
+        }
+    }
 
+    void print();
 };
-
 
 
 #endif /* TIMESERIES_H_ */
