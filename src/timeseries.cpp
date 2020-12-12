5f0b5170194ac95180712de4b109db6088667171
Eddie Gutman <edigutman@gmail.com>
2020-12-12 16:15:30 +0200

    Added documentation

--- src/timeseries.cpp
+++ src/timeseries.cpp
@@ -1,24 +1,28 @@
 #include <iostream>
 #include "timeseries.h"
 
-void TimeSeries::print() {
-    for (auto & it : Data) {
-        for (auto & ip : it.second) {
+// returns the number of features in the timeseries
+int TimeSeries::featureSize() const {
+    return Data.size();
+}
+
+// return the number of lines in the timeseries
+int TimeSeries::linesSize() const {
+    return Data.begin()->second.size();
+}
+
+// returns the value of line in feature s
+float TimeSeries::getValue(std::string s, int line) const {
+    return Data.find(s)->second[line];
+}
+
+// prints the timeseriess
+void TimeSeries::print() const {
+    for (auto &it : Data) {
+        for (auto &ip : it.second) {
             std::cout << ip << ' ';
         }
         std::cout << std::endl;
     }
-}
-
-int TimeSeries::featureSize() const{
-    return Data.size();
-}
-
-int TimeSeries::linesSize() const{
-    return Data.begin()->second.size();
-}
-
-float TimeSeries::getValue(std::string s, int line) const {
-    return Data.find(s)->second[line];
 }
 
--- src/timeseries.h
+++ src/timeseries.h
@@ -14,32 +14,54 @@ public:
 
     TimeSeries(const char *CSVfileName) {
         bool init = true;
+
+        // opening the csv file.
         std::ifstream file;
         file.open(CSVfileName);
         std::string line, cell;
+
+        // reading each line from the file into line.
         while (std::getline(file, line)) {
             std::vector<std::string> row;
             std::stringstream ss(line);
+
+            // if its the first line, its the features names.
             if (init) {
+
+                // split the line by ',' into 'cells'.
                 while (std::getline(ss, cell, ',')) {
+
+                    // create a vector for each feature and insert it into the map.
                     std::vector<float> numbers;
                     Data.insert(std::pair<std::string, std::vector<float>>(cell, numbers));
                 }
                 init = false;
                 continue;
             }
+
+            // if its float values.
             auto it = Data.begin();
+
+            // split the line by ',' into 'cells'.
             while (std::getline(ss, cell, ',')) {
+
+                // insert the float value into the correct vector.
                 it++->second.push_back(std::stof(cell));
             }
         }
     }
 
+    // returns the number of features in the timeseries
     int featureSize() const;
+
+    // return the number of lines in the timeseries
     int linesSize() const;
+
+    // returns the value of line in feature s
     float getValue(std::string s, int line) const;
 
-    void print();
+    // prints the timeseries
+    void print() const;
 };
 
 
